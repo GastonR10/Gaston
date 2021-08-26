0e16e07fead1d0d6be2d428f2f2254dcb927b9c8
GastonR10 <gastonrm10@gmail.com>
2021-08-26 20:49:29 -0300

    Clase 12

---
+++ Clase 11 Javascript/index.js
@@ -0,0 +1,19 @@
+// Objeto
+const cancion = {
+    duracion: {
+        minutos: 3,
+        segundos: 50,
+    },
+    autor: "César",
+    anho: 2021,
+    titulo: "La maravillosa vida del programador.",
+};
+
+console.log(cancion);
+console.log(cancion.duracion);
+console.log(cancion.duracion,minutos);
+
+// Arrays
+const newArray = ["Estudiante", "Programador", "Médico"]
+
+console.log(newArray[0])
