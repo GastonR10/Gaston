ca6c5ed57cb26a729fef24e0572cd3c9d1b454e4
Gaston Roberto <robertga@basfad.basf.net>
2021-09-14 20:28:57 -0300

    Clase 16

---
+++ Javascript/Clase 15/Lista de tareas/main.js
@@ -0,0 +1,21 @@
+const listaTareasU1 = document.getElementById("lista-tareas");
+
+const botonAgregar = document.getElementById("agregar");
+
+const descripcionInput = document.getElementById("tarea");
+
+const prioridadCombox = document.getElementById("prioridad")
+
+const creaerTarea = () => {
+    const tareaNueva = document.createElement("li")
+
+    tareaNueva.textContent = descripcionInput.value;
+
+    tareaNueva.classList.add(prioridadCombox.value);
+
+    console.log(prioridadCombox);
+
+    listaTareasU1.appendChild(tareaNueva);
+};
+
+botonAgregar.addEventListener("click", creaerTarea);
