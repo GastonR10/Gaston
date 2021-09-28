dec64875c801679ecfab46e263bb0c7dee5ee692
Gaston Roberto <robertga@basfad.basf.net>
2021-09-28 20:31:42 -0300

    Clase 19

---
+++ ServidorFalso/listaDeTareas/main.js
@@ -0,0 +1,24 @@
+const enterButton = document.getElementById("enter");
+const input = document.getElementById("userInput");
+const ul = document.querySelector("ul");
+
+function createListElement() {
+  const li = document.createElement("li");
+  li.textContent = input.value;
+  ul.appendChild(li);
+  input.value = "";
+
+  function tachar() {
+    li.classList.toggle("done");
+  }
+
+  li.addEventListener("click", tachar);
+}
+
+function addTarea() {
+  if (input.value.length > 0) {
+    createListElement();
+  }
+}
+
+enterButton.addEventListener("click", addTarea);
