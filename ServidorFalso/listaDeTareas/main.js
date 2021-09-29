ce169e4fc55f432fd3bf34a60569f5bd124d3830
Gaston Roberto <robertga@basfad.basf.net>
2021-09-28 23:25:04 -0300

    juego

--- ServidorFalso/listaDeTareas/main.js
+++ ServidorFalso/listaDeTareas/main.js
@@ -1,24 +1,45 @@
-const enterButton = document.getElementById("enter");
-const input = document.getElementById("userInput");
-const ul = document.querySelector("ul");
+const listaDeTareasUl = document.getElementById("lista-tareas");
+const inputTarea = document.getElementById("tarea");
+const selectorPrioridad = document.getElementById("prioridad");
+const botonAgregar = document.getElementById("agregar");
+const tareasConPrioridadBaja =
+  document.getElementsByClassName("prioridad-baja");
+const todosLosLi = document.getElementsByTagName("li");
 
-function createListElement() {
-  const li = document.createElement("li");
-  li.textContent = input.value;
-  ul.appendChild(li);
-  input.value = "";
+/* Creamos un elemento y lo colocamos en pantalla */
+function crearNuevaTarea(nombre, prioridad) {
+  // Creamos un nuevo elemento li y lo guardamos en una variable
+  const nuevaTarea = document.createElement("li");
 
-  function tachar() {
-    li.classList.toggle("done");
-  }
+  // Cambiamos el texto interno del li
+  nuevaTarea.textContent = nombre;
 
-  li.addEventListener("click", tachar);
+  // Agregamos una clase CSS al li
+  nuevaTarea.classList.add(prioridad);
+
+  // Agregamos el li a la lista de tareas (ul)
+  listaDeTareasUl.appendChild(nuevaTarea);
 }
 
-function addTarea() {
-  if (input.value.length > 0) {
-    createListElement();
-  }
-}
+// Agregamos un evento al boton "agregar" para que cuando se presione
+botonAgregar.addEventListener("click", function () {
+  crearNuevaTarea(inputTarea.value, selectorPrioridad.value);
+});
 
-enterButton.addEventListener("click", addTarea);
+listaDeTareasUl.addEventListener("click", function (evento) {
+  evento.target.remove();
+});
+
+function obtenerTareas() {
+  fetch("http://localhost:4000/tareas", {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((respuesta) => {
+      console.log(respuesta);
+      respuesta.map((item) => crearNuevaTarea(item.titulo, item.prioridad));
+    });
+}
+obtenerTareas();
+
+//function para traerme las tareas
